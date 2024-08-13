27d3d1feedf9cd941808e76277bfd87248c618da
Crownedtechie <nenchi00@gmail.com>
2024-08-13 13:03:24 +0100

    --style:: implement community page design

--- src/pages/Communities.jsx
+++ src/pages/Communities.jsx
@@ -1,5 +1,6 @@
 import Post from "@/Component/ui/Post";
 import Layout from "@/Component/Shared/Layout";
+import { postPic } from "@/assets";
 
 const Communities = () => {
   return (
@@ -7,14 +8,23 @@ const Communities = () => {
     <Layout>
         <header></header>
 
-        <section className="mt-10 mx-10 flex items-center justify-center">
-            <div className="grid grid-cols-[3fr_1fr] grid-rows-auto place-content-center gap-10 ">
-                <div>
+        <section className="mt-10 mx-10 flex items-center justify-center ">
+
+            <div className="grid grid-cols-[3fr_2fr] grid-rows-auto place-content-center">
+                <div className=" mr-10">
+                    {/* ALL POSTS WILL BE WITHIN THIS DIV */}
                     <h4 className="text-serene">Hot</h4>
-                    <Post></Post>
+                    <Post>
+                        <p>One thing that has really helped me is focusing on small, manageable steps. I started by setting tiny goals for myself, like getting out of bed at a certain time or going for a short walk. These might seem insignificant, but accomplishing them gave me a sense of achievement and gradually built up my confidence.</p>
+                    </Post>
+
+                    <Post>
+                        <p>Today felt like hell😥 I even considered suicide.</p>
+                        <img src={postPic} alt="post picture" className="my-3" />
+                    </Post>
                 </div>
 
-                <aside className="w-[18.8rem] h-[25rem] bg-[#272727] rounded-[9px] "></aside>
+                <aside className=" h-[25rem] bg-[#272727] rounded-[9px] "></aside>
             </div>
 
         </section>
